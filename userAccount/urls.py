fbe37a185dd1f9e630852ce07e32e9467f791ac0
Song Hyeon Sik <blogcin@naver.com>
2017-03-15 13:32:37 +0900

    Version up to django 1.10 and clean up codes
    Signed-off-by: Song Hyeon Sik <blogcin@naver.com>

--- userAccount/urls.py
+++ userAccount/urls.py
@@ -5,7 +5,6 @@ from django.contrib.auth.decorators import login_required
 from django.conf import settings
 
 urlpatterns =[
-	#url(r'^media/(?P<path>.*>$', login_required(serve), {'document_root': settings.MEDIA_ROOT}),
 	url(r'^sign/$', views.SignForm.as_view(), name='signForm'),
 	url(r'^login/$', views.LoginForm.as_view(), name='loginForm'),
 	url(r'^logout/$', views.logout, name='logout'),
@@ -13,6 +12,5 @@ urlpatterns =[
 	url(r'^index/$', views.IndexForm.as_view(), name='indexForm'),
 	url(r'^ranking/$', views.RankingForm.as_view(), name='rankingForm'),
 	url(r'^start/$', views.Start.as_view(), name='start'),
-	
-]
 
+]
